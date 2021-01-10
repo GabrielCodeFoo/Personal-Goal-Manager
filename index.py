f91971bd7c4a0f6bdef6b019f665af1d334cfaa1
gabriel.pilz <gabriel.pilz@klarna.com>
2021-01-10 16:55:52 +0100

    PGM-10 send a metric to CloudWatch

--- index.py
+++ index.py
@@ -1,12 +1,31 @@
 import json
 import datetime
+import boto3
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    cloudwatch = boto3.client('cloudwatch')
+    # metrics = [{e.metric_name, e.metric_value} for e in event]
+    if not event.metric_name:
+        raise Exception('Missing metric_name parameter')
+    if not event.metric_value:
+        raise Exception('Missing metric_value parameter')
+    if not event.user_id:
+        raise Exception('Missing user_id parameter')
+
+    response = cloudwatch.put_metric_data(
+        MetricData=[
+            {
+                'MetricName': event.metric_name,
+                'Dimensions': [
+                    {
+                        'Name': 'Name',
+                        'Value': event.user_id
+                    },
+                ],
+                'Unit': 'None',
+                'Value': event.metric_value
+            },
+        ],
+        Namespace='AWS/PGM'
+    )
